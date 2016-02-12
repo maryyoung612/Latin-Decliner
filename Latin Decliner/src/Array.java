312554285cc10c7d175b0e3c9da97d9fa7466781
maryyoung612 <mary.young@MU-C1-07.mullen.pvt.k12.co.us>
2016-02-12 09:10:07 -0700

    working on inport

--- Latin Decliner/src/Array.java
+++ Latin Decliner/src/Array.java
@@ -1,13 +1,24 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Scanner;
 
 public class Array
 	{
-	static ArrayList <Word> q = new ArrayList <Word>();
 	static ArrayList <Declension> s= new ArrayList <Declension>();
 	static ArrayList <Declension> c= new ArrayList <Declension>();
-	public static void makeArray()
+	
+	public static void makeArray() throws IOException
 		{
-		q.add(new Word("", "", ""));
+		Scanner file = new Scanner(new File("Dictionary.txt"));
+		while(File.hasNext())
+			{
+				
+			}
+		ArrayList <Word> importW = new ArrayList <Word>();
+		importW.add(new Word("","",""));
+		importW.add(new Word("","",""));
 		}
 	public static void makeSupArray()
 		{
