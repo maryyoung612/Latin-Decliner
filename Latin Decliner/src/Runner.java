037865184d56f5a58c91caa09686a5aff98da21f
Mary <Mary@192.168.2.7>
2016-02-16 07:21:40 -0700

    fin 2

--- Latin Decliner/src/Runner.java
+++ Latin Decliner/src/Runner.java
@@ -17,59 +17,62 @@ public class Runner
 				if(Array.importW.get(i).getLatinWord1().equals(input))
 					{
 					System.out.println(Array.importW.get(i));
-					
-					if(Array.importW.get(i).getDelNum().equals("first"))
-					{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						First.isFemineFi();
-					}
-					if(Array.importW.get(i).getDelNum().equals("second"))
-					{
-						if(Array.importW.get(i).getGender().equals("m"))
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord1().length()-2);
-						Second.isMasculineS();
-						}
-						else
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord1().length()-2);
-						Second.isNeuterS();
-						}
-					}
-					if(Array.importW.get(i).getDelNum().equals("third"))
-					{
-						if(Array.importW.get(i).getGender().equals("m")||Array.importW.get(i).getGender().equals("f"))
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						Third.isGenderT();
-						}
-						else
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						Third.isNeuterT();
-						}
-					}
-					if(Array.importW.get(i).getDelNum().equals("fourth"))
-					{
-						if(Array.importW.get(i).getGender().equals("m")||Array.importW.get(i).getGender().equals("f"))
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						Fourth.isMasculineFo();
-						}
-						else
-						{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						Fourth.isNeuterFo();
-						}
-					}
-					if(Array.importW.get(i).getDelNum().equals("fifth"))
-					{
-						root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
-						Fifth.isFif();		
-					}
+					decline(i);
 					}
 				}
 			}
+public static void decline(int i)
+		{
+	if(Array.importW.get(i).getDelNum().equals("first"))
+	{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		First.isFemineFi();
+	}
+	if(Array.importW.get(i).getDelNum().equals("second"))
+	{
+		if(Array.importW.get(i).getGender().equals("m"))
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord1().length()-2);
+		Second.isMasculineS();
+		}
+		else
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord1().length()-2);
+		Second.isNeuterS();
+		}
+	}
+	if(Array.importW.get(i).getDelNum().equals("third"))
+	{
+		if(Array.importW.get(i).getGender().equals("m")||Array.importW.get(i).getGender().equals("f"))
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		Third.isGenderT();
+		}
+		else
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		Third.isNeuterT();
+		}
+	}
+	if(Array.importW.get(i).getDelNum().equals("fourth"))
+	{
+		if(Array.importW.get(i).getGender().equals("m")||Array.importW.get(i).getGender().equals("f"))
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		Fourth.isMasculineFo();
+		}
+		else
+		{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		Fourth.isNeuterFo();
+		}
+	}
+	if(Array.importW.get(i).getDelNum().equals("fifth"))
+	{
+		root = Array.importW.get(i).getLatinWord2().substring(0,Array.importW.get(i).getLatinWord2().length()-2);
+		Fifth.isFif();		
+	}
+		}
 public static void findSupCom()
 	{
 	if(input.endsWith("ior"))
